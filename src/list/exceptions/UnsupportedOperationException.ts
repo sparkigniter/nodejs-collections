5517cb61b6178ec5b740fb82a15abe630536756e
vikas <vickybgoudar@gmail.com>
2024-03-04 13:16:50 +0530

    Implemented Addall function

---
+++ src/list/exceptions/UnsupportedOperationException.ts
@@ -0,0 +1,8 @@
+class UnsupportedOperationException extends Error {
+    constructor(message: string){
+        super(message);
+        this.name = this.constructor.name;
+    }
+}
+
+export default UnsupportedOperationException;
